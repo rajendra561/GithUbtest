fbce912925043e1a60c8a23bd0e7e16ab1a78544
rajendra561 <rajendra.asuri@gmail.com>
2021-06-06 15:46:31 +0530

    Initial Commit

---
+++ GithUbtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithUbtest
+//
+//  Created by Saudi Electricity Company on 06/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
